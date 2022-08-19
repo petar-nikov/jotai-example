5fa70ac4eb6d00b9de5c1eee724a7cad252f1386
Petar Nikov <pnikov@developsoft.com>
2022-08-19 11:49:44 +0300

    feat: move filter logic from the component to the model.

--- src/components/PokemonTable/PokemonTable.tsx
+++ src/components/PokemonTable/PokemonTable.tsx
@@ -1,9 +1,8 @@
 import { useAtom } from 'jotai'
-import { filterAtom, pokemmonAtom } from "store"
+import { filteredPokemonAtom } from "store"
 
 export const PokemonTable = () => {
-  const [pokemon] = useAtom(pokemmonAtom)
-  const [filter] = useAtom(filterAtom)
+  const [filtered] = useAtom(filteredPokemonAtom)
   return (
     <table className="w-full">
       <tbody className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
@@ -11,7 +10,7 @@ export const PokemonTable = () => {
           <th scope="col" className="px-3 py-4">Name</th>
           <th scope="col" className="px-3 py-4">Type</th>
         </tr>
-        {pokemon.filter(p => p.name.english.toLowerCase().includes(filter.toLowerCase()))
+        {filtered
           .map((p)=> (
           <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <td className="px-3 py-4">{p.name.english}</td>
--- src/store/AppStore.tsx
+++ src/store/AppStore.tsx
@@ -2,3 +2,7 @@ import { atom } from 'jotai';
 
 export const pokemmonAtom = atom<any[]>([])
 export const filterAtom = atom<string>('')
+export const filteredPokemonAtom = atom( get => 
+get(pokemmonAtom)
+  .filter(p => p.name.english.toLowerCase().includes(get(filterAtom).toLowerCase()))
+)
