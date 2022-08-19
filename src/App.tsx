164600c361f253bb9d251556acec266a3fcf46bd
Petar Nikov <pnikov@developsoft.com>
2022-08-19 09:40:04 +0300

    chore: add jotai

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import {atom, Provider, useAtom} from 'jotai';
+
+const pokemmonAtom = atom<any[]>([])
+const filterAtom = atom<string>('')
+
+function FilterInput() {
+  const [filter, updateFilter] = useAtom(filterAtom);
+  const inputClasses = "text-sm sm:text-base relative w-full border rounded-lg placeholder-gray-400 focus:border-indigo-400 focus:outline-none py-2 pr-2 pl-12"
+  return <input className={inputClasses} type="text" value={filter} onChange={(e) => updateFilter(e.target.value)} />
+}
 
 function App() {
-  return <main className="w-full h-screen flex items-center justify-start">App</main>
+  const [filter] = useAtom(filterAtom);
+  return <main className="w-96 mx-auto mt-10"><FilterInput /> {filter} </main>
 }
 
 export default App
