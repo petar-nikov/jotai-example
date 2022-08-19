3efa3ca99c29265b5928a0142a9666caad3439d9
Petar Nikov <pnikov@developsoft.com>
2022-08-19 10:36:14 +0300

    fix: update structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import {atom, Provider, useAtom} from 'jotai';
-
-const pokemmonAtom = atom<any[]>([])
-const filterAtom = atom<string>('')
-
-function FilterInput() {
-  const [filter, updateFilter] = useAtom(filterAtom);
-  const inputClasses = "text-sm sm:text-base relative w-full border rounded-lg placeholder-gray-400 focus:border-indigo-400 focus:outline-none py-2 pr-2 pl-12"
-  return <input className={inputClasses} type="text" value={filter} onChange={(e) => updateFilter(e.target.value)} />
-}
+import {Provider, useAtom} from 'jotai'
+import {filterAtom} from 'store'
+import { FilterInput } from 'components'
 
 function App() {
   const [filter] = useAtom(filterAtom);
   return <main className="w-96 mx-auto mt-10"><FilterInput /> {filter} </main>
 }
 
-export default App
+export default () => <Provider> <App /> </Provider>
